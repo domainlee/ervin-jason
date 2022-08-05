ad14f2e1eecef9a48d989615f440d801a6d62c7b
Le Mien <lemien@Les-MacBook-Air.local>
2022-08-05 17:01:04 +0700

    u

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,6 +15,7 @@
             $('body').delay(666);
             animation();
             skill();
+            typing();
         });
     }
 
@@ -22,6 +23,22 @@
         $('.loading').css('display', 'none');
         animation();
         skill();
+        typing();
+    }
+
+    var typing = function () {
+        var elements = document.getElementsByClassName('type--js');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+                new TextType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".type--js > .wrap { border-right: 0.08em solid #000000}";
+        document.body.appendChild(css);
     }
 
     var slider = function () {
@@ -328,6 +345,41 @@
             $('html').attr("data-theme", "dark");
             localStorage.setItem("data-theme", "dark")
         }
+    }
+
+    var TextType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    }
+
+    TextType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+        if (this.isDeleting) {
+            this.txt = fullTxt.substring(0, this.txt.length - 1)
+        } else {
+            this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+        if (this.isDeleting) { delta /= 2; }
+        if (!this.isDeleting && this.txt === fullTxt) {
+            delta = this.period;
+            this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+            this.isDeleting = false;
+            this.loopNum++;
+            delta = 500;
+        }
+        setTimeout(function() {
+            that.tick();
+        }, delta);
     }
 
     $(document).ready(function() {
